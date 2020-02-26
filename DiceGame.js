33b08a1462e8db2f3bc19abed2e8b49aa63b0590
Pablo Gros <rob.r.pete@gmail.com>
2020-02-26 16:42:03 -0500

    all players dice roll is updated below player name

--- DiceGame.js
+++ DiceGame.js
@@ -1,6 +1,6 @@
-var p1 = 1, p2 = 1, p3 = 1,p4 = 1,p5 = 1,p6 = 1,p7 = 1,p8 = 1,p8 = 1,p10 = 1;
-var p1Score = 0, p2Score = 0, p3Score = 0, p4Score = 0, p5Score = 0, p6Score = 0, p7Score = 0, p8Score = 0, p9Score = 0, p10Score = 0;
-
+var players = ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"];
+var playerScore = [1,1,1,1,1,1,1,1,1,1]
+var diceTotal = 0;
 function StartGame(){
     ShowPlayers()
     ShowPlayerScore()
@@ -20,7 +20,7 @@ function DiceRoll(){
     var d4 = Math.floor(Math.random() * 10) + 1;
     var d5 = Math.floor(Math.random() * 12) + 1;
     var d6 = Math.floor(Math.random() * 20) + 1;
-    var diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
+    diceTotal = d1 + d2 + d3 + d4 + d5 + d6;
     dice1.innerHTML = d1;
     dice2.innerHTML = d2;
     dice3.innerHTML = d3;
@@ -34,71 +34,84 @@ function DiceRoll(){
 }
 
 function ShowPlayers(){
-    if(p1 !== 0){
-        document.getElementById("p1").innerHTML = "P1";
+    if(playerScore[0] !== 0){
+        document.getElementById("p1").innerHTML = players[0];
     }
-    if(p2 !== 0){
-        document.getElementById("p2").innerHTML = "P2";
+    if(playerScore[1] !== 0){
+        document.getElementById("p2").innerHTML = players[1];
     }
-    if(p3 !== 0){
-        document.getElementById("p3").innerHTML = "P3";
+    if(playerScore[2] !== 0){
+        document.getElementById("p3").innerHTML = players[2];
     }
-    if(p4 !== 0){
-        document.getElementById("p4").innerHTML = "P4";
+    if(playerScore[3] !== 0){
+        document.getElementById("p4").innerHTML = players[3];
     }
-    if(p5 !== 0){
-        document.getElementById("p5").innerHTML = "P5";
+    if(playerScore[4] !== 0){
+        document.getElementById("p5").innerHTML = players[4];
     }
-    if(p6 !== 0){
-        document.getElementById("p6").innerHTML = "P6";
+    if(playerScore[5] !== 0){
+        document.getElementById("p6").innerHTML = players[5];
     }
-    if(p7 !== 0){
-        document.getElementById("p7").innerHTML = "P7";
+    if(playerScore[6] !== 0){
+        document.getElementById("p7").innerHTML = players[6];
     }
-    if(p8 !== 0){
-        document.getElementById("p8").innerHTML = "P8";
+    if(playerScore[7] !== 0){
+        document.getElementById("p8").innerHTML = players[7];
     }
-    if(p9 !== 0){
-        document.getElementById("p9").innerHTML = "P9";
+    if(playerScore[8] !== 0){
+        document.getElementById("p9").innerHTML = players[8];
     }
-    if(p10 !== 0){
-        document.getElementById("p10").innerHTML = "P10";
+    if(playerScore[9] !== 0){
+        document.getElementById("p10").innerHTML = players[9];
     }
 }
 
 function ShowPlayerScore(){
-    if(p1 !== 0){
-        document.getElementById("p1Score").innerHTML = p1Score;
+    if(playerScore[0] !== 0){
+        document.getElementById("p1Score").innerHTML = " ";
     }
-    if(p2 !== 0){
-        document.getElementById("p2Score").innerHTML = p2Score;
-    }if(p3 !== 0){
-        document.getElementById("p3Score").innerHTML = p3Score;
+    if(playerScore[1] !== 0){
+        document.getElementById("p2Score").innerHTML = " ";
     }
-    if(p4 !== 0){
-        document.getElementById("p4Score").innerHTML = p4Score;
+    if(playerScore[2] !== 0){
+        document.getElementById("p3Score").innerHTML = " ";
     }
-    if(p5 !== 0){
-        document.getElementById("p5Score").innerHTML = p5Score;
+    if(playerScore[3] !== 0){
+        document.getElementById("p4Score").innerHTML = " ";
     }
-    if(p6 !== 0){
-        document.getElementById("p6Score").innerHTML = p6Score;
+    if(playerScore[4] !== 0){
+        document.getElementById("p5Score").innerHTML = " ";
     }
-    if(p7 !== 0){
-        document.getElementById("p7Score").innerHTML = p7Score;
+    if(playerScore[5] !== 0){
+        document.getElementById("p6Score").innerHTML = " ";
     }
-    if(p8 !== 0){
-        document.getElementById("p8Score").innerHTML = p8Score;
+    if(playerScore[6] !== 0){
+        document.getElementById("p7Score").innerHTML = " ";
     }
-    if(p9 !== 0){
-        document.getElementById("p9Score").innerHTML = p9Score;
+    if(playerScore[7] !== 0){
+        document.getElementById("p8Score").innerHTML = " ";
     }
-    if(p10 !== 0){
-        document.getElementById("p10Score").innerHTML = p10Score;
+    if(playerScore[8] !== 0){
+        document.getElementById("p9Score").innerHTML = " ";
+    }
+    if(playerScore[9] !== 0){
+        document.getElementById("p10Score").innerHTML = " ";
     }
 }
 
-function Round(){
-    p1Score = diceTotal;
-    p1Score.innerHTML = p1Score;
+function SetPlayersScoreOnRoll(){
+    for(let i = 0; i < players.length; i++){
+        DiceRoll()
+        playerScore[i] = diceTotal;
+        document.getElementById("p1Score").innerHTML = playerScore[0];
+        document.getElementById("p2Score").innerHTML = playerScore[1];
+        document.getElementById("p3Score").innerHTML = playerScore[2];
+        document.getElementById("p4Score").innerHTML = playerScore[3];
+        document.getElementById("p5Score").innerHTML = playerScore[4];
+        document.getElementById("p6Score").innerHTML = playerScore[5];
+        document.getElementById("p7Score").innerHTML = playerScore[6];
+        document.getElementById("p8Score").innerHTML = playerScore[7];
+        document.getElementById("p9Score").innerHTML = playerScore[8];
+        document.getElementById("p10Score").innerHTML = playerScore[9];
+    }
 }
